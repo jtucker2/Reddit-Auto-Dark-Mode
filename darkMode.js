76e73e2805000d776102c9f0315061073da6a944
jtucker2 <joshuatucker648@gmail.com>
2022-05-31 17:17:56 +0100

    Theme now only switched if themes are misaligned and menu is closed after changing theme

--- darkMode.js
+++ darkMode.js
@@ -2,17 +2,25 @@ const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-schem
 
 console.log(prefersDark);
 
-var dropDownButton = document.getElementById("USER_DROPDOWN_ID");
-dropDownButton.click();
-var body = document.body;
-dropDown = body.lastChild.firstChild;
-var possibles = dropDown.firstChild.childNodes;
+switchTheme();
 
-for (var possible of Array.from(possibles)) {
-    if (possible.getAttribute("class") == "_3SDj_IT6ZaqCbKfC4eTjb2") {
-        if (possible.firstChild.firstChild.innerText == "Dark Mode") {
-            possible.firstChild.click();
-            break;
+function switchTheme() {
+    var dropDownButton = document.getElementById("USER_DROPDOWN_ID");
+    dropDownButton.click();
+    var body = document.body;
+    dropDown = body.lastChild.firstChild;
+    var possibles = dropDown.firstChild.childNodes;
+    
+    for (var possible of Array.from(possibles)) {
+        if (possible.getAttribute("class") == "_3SDj_IT6ZaqCbKfC4eTjb2") {
+            if (possible.firstChild.firstChild.innerText == "Dark Mode") {
+                if (possible.firstChild.lastChild.getAttribute("aria-checked") != toString(prefersDark)) {
+                    possible.firstChild.click();
+                }
+                break;
+            }
         }
     }
+    
+    dropDownButton.click();
 }
