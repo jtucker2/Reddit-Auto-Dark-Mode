1259b6a9ac5fe6538897c1559770c44f263fcbf1
jtucker2 <joshuatucker648@gmail.com>
2022-06-01 10:33:51 +0100

    Code no longer looks for class name when searching through user dropdown elements

--- darkMode.js
+++ darkMode.js
@@ -13,21 +13,22 @@ function switchTheme() {
     var userDropDownButton = document.getElementById("USER_DROPDOWN_ID");
     userDropDownButton.click();
 
+    // When opened, the user dropdown always seems to be the last element in the document body
     var userDropDown = document.body.lastChild.firstChild;
     var userDropDownElements = userDropDown.firstChild.childNodes;
     
     for (var element of Array.from(userDropDownElements)) {
-        if (element.getAttribute("class") == "_3SDj_IT6ZaqCbKfC4eTjb2") {
-            if (element.firstChild.firstChild.innerText == "Dark Mode") {
-                var isRedditDark = element.firstChild.lastChild.getAttribute("aria-checked");
 
-                if (isRedditDark != isSystemDark.toString()) {
-                    element.firstChild.click();
-                }
+        var elementButton = element.firstChild;
 
-                break;
-            }
+        if (elementButton.firstChild.innerText == "Dark Mode") {
+
+            var isRedditDark = elementButton.lastChild.getAttribute("aria-checked");
+            if (isRedditDark != isSystemDark.toString()) elementButton.click();
+            break;
+            
         }
+
     }
 
     userDropDownButton.click();
