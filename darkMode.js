b6d6dd5172ade2b695720ab6ea174cfaa44eebc6
jtucker2 <joshuatucker648@gmail.com>
2022-06-01 11:53:28 +0100

    Fixed theme sometimes not changing if JS for opening user drop down hadn't loaded before the sript ran

--- darkMode.js
+++ darkMode.js
@@ -1,6 +1,17 @@
 const darkThemeMq = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)');
 var isSystemDark = darkThemeMq.matches;
 
+
+var userDropDownButton = document.getElementById("USER_DROPDOWN_ID");
+
+function openUserDropDown() {
+    if (userDropDownButton.getAttribute("aria-expanded") == "false") userDropDownButton.click();
+}
+function closeUserDropDown() {
+    if (userDropDownButton.getAttribute("aria-expanded") == "true") userDropDownButton.click();
+}
+
+
 switchTheme();
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function(e) { 
@@ -8,28 +19,47 @@ window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', fun
     switchTheme();
 })
 
+
 // Switches reddit's theme if it doesn't match the system theme
 function switchTheme() {
-    var userDropDownButton = document.getElementById("USER_DROPDOWN_ID");
-    userDropDownButton.click();
 
-    // When opened, the user dropdown always seems to be the last element in the document body
-    var userDropDown = document.body.lastChild.firstChild;
-    var userDropDownElements = userDropDown.firstChild.childNodes;
+    const observer = new MutationObserver(function(mutationList, observer) {
+
+        /* Sometimes the JavaScript that opens the drop down hasn't loaded in time for the first 
+        click event so we try again here in case it hasn't opened */
+        openUserDropDown();
+
+        var userDropDown = document.body.lastChild.firstChild;
+
+        // When opened, the user dropdown always seems to be the last element in the document body
+        if (userDropDown != null && userDropDown.getAttribute("class") == "_2uYY-KeuYHKiwl-9aF0UiL _1HSQGYlfPWzs40LP4_oFi5 _2XkHtsPtFuTExJyk9JQUAp") {
+
+            var userDropDownElements = userDropDown.firstChild.childNodes;
+        
+            for (var element of Array.from(userDropDownElements)) {
     
-    for (var element of Array.from(userDropDownElements)) {
+                var elementButton = element.firstChild;
+    
+                if (elementButton.firstChild.innerText == "Dark Mode") {
+    
+                    var isRedditDark = elementButton.lastChild.getAttribute("aria-checked");
+                    if (isRedditDark != isSystemDark.toString()) elementButton.click();
+                    break;
+                    
+                }
+    
+            }
+    
+            closeUserDropDown();
+            observer.disconnect();
 
-        var elementButton = element.firstChild;
-
-        if (elementButton.firstChild.innerText == "Dark Mode") {
-
-            var isRedditDark = elementButton.lastChild.getAttribute("aria-checked");
-            if (isRedditDark != isSystemDark.toString()) elementButton.click();
-            break;
-            
         }
 
-    }
+    });
 
-    userDropDownButton.click();
+    observer.observe(document.body, {childList: true});
+
+    // Ensures there is a DOM change for the observer in case nothing is happening on the page
+    openUserDropDown();
+
 }
